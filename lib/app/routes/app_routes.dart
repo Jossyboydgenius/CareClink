dde523923b3a8e3c99db0d07109eb61511be2f88
jossyboydgenius <jossyboydgenius@gmail.com>
2025-01-26 10:14:08 +0000

    Refactor main.dart to update initial route and routes

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../../ui/views/sign_in_view.dart';
+import '../../ui/views/splash_screen_view.dart';
+import '../../ui/views/dashboard_view.dart';
+import '../../ui/views/notification_view.dart';
+import '../../ui/views/appointment_view.dart';
+
+class AppRoutes {
+  static const String splashScreenView = '/';
+  static const String signInView = '/signIn';
+  static const String dashboardView = '/dashboard';
+  static const String notificationView = '/notification';
+  static const String appointmentView = '/appointment';
+
+  static const String initialRoute = splashScreenView;
+
+  static Map<String, WidgetBuilder> routes = {
+    splashScreenView: (context) => const SplashScreen(),
+    signInView: (context) => const SignInView(),
+    dashboardView: (context) => const Dashboard(),
+    notificationView: (context) => const NotificationView(),
+    appointmentView: (context) => const AppointmentView(),
+  };
+} 
