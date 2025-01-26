a8a0b78eff40fc3305e1f5868aaa39e37c4528d2
jossyboydgenius <jossyboydgenius@gmail.com>
2025-01-26 06:42:06 +0000

    Refactor AppTextStyle to use the Inter font and manage text styles
    This commit refactors the AppTextStyle class to use the Inter font for consistent typography throughout the app. It also introduces various text styles with different font sizes and weights for different UI elements. The color palette is now managed through the AppColors class. The flutter_screenutil package has been added to the pubspec.lock file to support responsive font sizes.

--- lib/shared/app_text_style.dart
+++ lib/shared/app_text_style.dart
@@ -1,11 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'app_colors.dart';
 
 abstract class AppTextStyle {
   /// Base text style
   static const TextStyle _baseTextStyle = TextStyle(
     fontFamily: 'Inter',
     fontWeight: AppFontWeight.regular,
-    color: Color(0xFF404969),
+    color: AppColors.textPrimary,
+  );
+
+  ///Inter 10
+  static TextStyle get regular10 => _baseTextStyle.copyWith(fontSize: 10);
+
+  ///Inter medium 10
+  static TextStyle get medium10 =>
+      regular10.copyWith(fontWeight: AppFontWeight.medium);
+
+  ///Inter 12
+  static TextStyle get regular12 => _baseTextStyle.copyWith(fontSize: 12);
+
+  ///Inter medium 12
+  static TextStyle get medium12 =>
+      regular12.copyWith(fontWeight: AppFontWeight.medium);
+
+  ///Inter semibold 12
+  static TextStyle get semibold12 =>
+      regular12.copyWith(fontWeight: AppFontWeight.semiBold);
+
+  ///Inter medium 14
+  static TextStyle get medium14 => regular10.copyWith(
+        fontWeight: AppFontWeight.medium,
+        fontSize: 14.spMin,
+      );
+
+  ///Inter medium 16
+  static TextStyle get medium16 => regular10.copyWith(
+        fontWeight: AppFontWeight.medium,
+        fontSize: 16.spMin,
+      );
+
+  ///Inter 14
+  static TextStyle get regular14 => _baseTextStyle.copyWith(fontSize: 14);
+
+  ///Inter light 14
+  static TextStyle get light14 =>
+      regular14.copyWith(fontWeight: AppFontWeight.light);
+
+  ///Inter semibold 14
+  static TextStyle get semibold14 =>
+      regular14.copyWith(fontWeight: AppFontWeight.semiBold);
+
+  ///Inter bold 14
+  static TextStyle get bold14 =>
+      regular14.copyWith(fontWeight: AppFontWeight.bold);
+
+  ///Inter 15
+  static TextStyle get regular15 => _baseTextStyle.copyWith(fontSize: 15);
+
+  ///Inter medium 15
+  static TextStyle get medium15 =>
+      regular15.copyWith(fontWeight: AppFontWeight.medium);
+
+  ///Inter semibold 15
+  static TextStyle get semibold15 =>
+      regular15.copyWith(fontWeight: AppFontWeight.semiBold);
+
+  ///Inter 16
+  static TextStyle get regular16 => _baseTextStyle.copyWith(fontSize: 16);
+
+  ///Inter light 16
+  static TextStyle get light16 =>
+      regular16.copyWith(fontWeight: AppFontWeight.light);
+
+  ///Inter semibold 16
+  static TextStyle get semibold16 =>
+      regular16.copyWith(fontWeight: AppFontWeight.semiBold);
+
+  ///Inter bold 16
+  static TextStyle get bold16 =>
+      regular16.copyWith(fontWeight: AppFontWeight.bold);
+
+  ///Inter 18
+  static TextStyle get regular18 => _baseTextStyle.copyWith(fontSize: 18);
+
+  ///Inter semibold 18
+  static TextStyle get semibold18 =>
+      regular18.copyWith(fontWeight: AppFontWeight.semiBold);
+
+  ///Inter bold 18
+  static TextStyle get bold18 =>
+      regular18.copyWith(fontWeight: AppFontWeight.bold);
+
+  ///Inter 20
+  static TextStyle get regular20 => _baseTextStyle.copyWith(fontSize: 20);
+
+  ///Inter medium 20
+  static TextStyle get medium20 =>
+      regular20.copyWith(fontWeight: AppFontWeight.medium);
+
+  ///Inter semibold 20
+  static TextStyle get semibold20 =>
+      regular20.copyWith(fontWeight: AppFontWeight.semiBold);
+
+  ///Inter bold 20
+  static TextStyle get bold20 =>
+      regular20.copyWith(fontWeight: AppFontWeight.bold);
+
+  ///Inter semibold 24
+  static TextStyle get semibold24 => regular16.copyWith(
+        fontWeight: AppFontWeight.semiBold,
+        fontSize: 24.spMin,
+      );
+
+  ///Inter bold 24
+  static const TextStyle bold24 = TextStyle(
+    fontWeight: AppFontWeight.bold,
+    fontSize: 24,
   );
 
   // Welcome Back text style
@@ -46,6 +157,62 @@ abstract class AppTextStyle {
         height: 16/12, // line-height: 16px
         letterSpacing: 0.005,
         color: Colors.grey,
+      );
+
+  // Timesheet styles
+  static TextStyle get timesheetTitle => _baseTextStyle.copyWith(
+        fontSize: 20,
+        fontWeight: AppFontWeight.semiBold,
+        height: 24/20, // line-height: 24px
+      );
+
+  static TextStyle get visitId => _baseTextStyle.copyWith(
+        fontSize: 14,
+        fontWeight: AppFontWeight.semiBold,
+        height: 20/14, // line-height: 20px
+      );
+
+  static TextStyle get timesheetLabel => _baseTextStyle.copyWith(
+        fontSize: 12,
+        fontWeight: AppFontWeight.regular,
+        height: 16/12, // line-height: 16px
+        letterSpacing: 0.005,
+      );
+
+  static TextStyle get timesheetTime => _baseTextStyle.copyWith(
+        fontSize: 12,
+        fontWeight: AppFontWeight.semiBold,
+        height: 16/12, // line-height: 16px
+      );
+
+  static TextStyle get clockOutButton => _baseTextStyle.copyWith(
+        fontSize: 12,
+        fontWeight: AppFontWeight.medium,
+        height: 16/12, // line-height: 16px
+        letterSpacing: 0.005,
+        color: AppColors.primary,
+      );
+
+  static TextStyle get appointmentDetails => _baseTextStyle.copyWith(
+        fontSize: 12,
+        fontWeight: AppFontWeight.medium,
+        height: 16/12, // line-height: 16px
+        letterSpacing: 0.005,
+        color: AppColors.grey300,
+      );
+
+  static TextStyle get durationLabel => _baseTextStyle.copyWith(
+        fontSize: 12,
+        fontWeight: AppFontWeight.medium,
+        height: 16/12, // line-height: 16px
+        letterSpacing: 0.005,
+      );
+
+  static TextStyle get durationValue => _baseTextStyle.copyWith(
+        fontSize: 12,
+        fontWeight: AppFontWeight.semiBold,
+        height: 16/12, // line-height: 16px
+        letterSpacing: 0.005,
       );
 }
 
