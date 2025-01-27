be86a0a4f68c6c0332c62c3f98f277e96779faec
jossyboydgenius <jossyboydgenius@gmail.com>
2025-01-27 19:48:53 +0000

    Refactor AppCheckbox widget to use Checkbox widget and update styling

--- lib/ui/widgets/app_checkbox.dart
+++ lib/ui/widgets/app_checkbox.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../shared/app_colors.dart';
 
 class AppCheckbox extends StatelessWidget {
@@ -14,26 +13,20 @@ class AppCheckbox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onChanged(!value),
-      child: Container(
-        width: 20.w,
-        height: 20.w,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: value ? AppColors.primary : Colors.transparent,
-          border: Border.all(
-            color: value ? AppColors.primary : AppColors.grey400,
-            width: 1.5,
-          ),
+    return SizedBox(
+      width: 20,
+      height: 20,
+      child: Checkbox(
+        value: value,
+        onChanged: onChanged,
+        activeColor: AppColors.primary,
+        side: BorderSide(
+          color: AppColors.grey400,
+          width: 1,
         ),
-        child: value
-            ? Icon(
-                Icons.check,
-                size: 16.w,
-                color: Colors.white,
-              )
-            : null,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
       ),
     );
   }
