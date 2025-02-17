58f048f38d7c1b044bee3abf414056fd5f4abbfa
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-17 17:23:41 +0000

    Add MainApplication.kt file for CareClink Flutter application

---
+++ android/app/src/main/kotlin/com/carelink/app/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.carelink.app
+
+import io.flutter.app.FlutterApplication
+
+class MainApplication: FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
