c4552006fe10bea95bdbd50047698d61f5093704
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-17 17:23:49 +0000

    Reduce splash screen delay from 3 seconds to 2 seconds

--- lib/ui/views/splash_screen_view.dart
+++ lib/ui/views/splash_screen_view.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const SignInView()),
@@ -35,4 +35,4 @@ class _SplashScreenState extends State<SplashScreen> {
       ),
     );
   }
-} 
+}
