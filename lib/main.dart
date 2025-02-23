eadc1ed3f3e96eb48baddc946f6a950a7c9ab70c
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-23 07:22:56 +0000

    Refactor main function to load environment variables and initialize locator with AppFlavorConfig; integrate MultiBlocProvider for SignInBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/routes/app_routes.dart';
 import 'app/themes.dart';
 import 'data/services/navigator_service.dart';
 import 'app/locator.dart';
+import 'app/flavor_config.dart';
+import 'ui/views/sign_in_bloc/sign_in_bloc.dart';
+import 'shared/app_sizer.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await setupLocator();
+  await dotenv.load(fileName: ".env");
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  await setupLocator(AppFlavorConfig(
+    name: 'CareClink',
+    apiBaseUrl: dotenv.env['BASE_URL_PROD']!,
+    webUrl: dotenv.env['WEB_URL_PROD']!,
+    mixpanelToken: dotenv.env['MIXPANEL_TOKEN_PROD']!,
+  ));
+
   runApp(const MyApp());
 }
 
@@ -16,18 +35,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AppDimension.init(context);
     return ScreenUtilInit(
-      designSize: const Size(375, 812), // Base design size (iPhone 12/13)
+      designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (_, child) {
-        return MaterialApp(
-          title: 'CareClink',
-          debugShowCheckedModeBanner: false,
-          theme: AppTheme.light,
-          navigatorKey: NavigationService.navigatorKey,
-          initialRoute: AppRoutes.signInView,
-          routes: AppRoutes.routes,
+      builder: (BuildContext context, Widget? child) {
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (context) => SignInBloc()),
+          ],
+          child: MaterialApp(
+            title: 'CareClink',
+            navigatorKey: NavigationService.navigatorKey,
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.light,
+            initialRoute: AppRoutes.signInView,
+            routes: AppRoutes.routes,
+            builder: (context, child) {
+              ScreenUtil.init(context);
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: child!,
+              );
+            },
+          ),
         );
       },
     );
