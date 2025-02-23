92de50193b42f4514f55f734c95bea191410ac47
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-23 04:49:34 +0000

    Add service locator setup for dependency injection and environment configuration

---
+++ lib/app/locator.dart
@@ -0,0 +1,23 @@
+import 'package:get_it/get_it.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../data/services/api/api.dart';
+import '../data/services/user_service.dart';
+import 'flavor_config.dart';
+
+final locator = GetIt.instance;
+
+Future<void> setUpLocator() async {
+  await dotenv.load(fileName: '.env');
+  
+  final config = AppFlavorConfig(
+    name: dotenv.env['APP_NAME'] ?? 'development',
+    apiBaseUrl: dotenv.env['API_BASE_URL'] ?? '',
+    webUrl: dotenv.env['WEB_URL'] ?? '',
+    sentryDsn: dotenv.env['SENTRY_DSN'] ?? '',
+    mixpanelToken: dotenv.env['MIXPANEL_TOKEN'] ?? '',
+  );
+  
+  locator.registerLazySingleton<AppFlavorConfig>(() => config);
+  locator.registerLazySingleton<Api>(() => Api());
+  locator.registerLazySingleton<UserService>(() => UserService());
+} 
