063f57f3c5b85b4d93d7903b9acd8133f7609afe
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-23 05:25:44 +0000

    Remove unused Sentry DSN from AppFlavorConfig

--- lib/app/flavor_config.dart
+++ lib/app/flavor_config.dart
@@ -2,14 +2,12 @@ class AppFlavorConfig {
   final String name;
   final String apiBaseUrl;
   final String webUrl;
-  final String sentryDsn;
   final String mixpanelToken;
 
   AppFlavorConfig({
     required this.name,
     required this.apiBaseUrl,
     required this.webUrl,
-    required this.sentryDsn,
     required this.mixpanelToken,
   });
 }
