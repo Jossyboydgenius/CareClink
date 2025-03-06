d4ba2a2b80fc5f139f467696948afa4038cb00de
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-06 04:33:45 +0000

    Add vertical spacing constants to AppSpacing

--- lib/shared/app_spacing.dart
+++ lib/shared/app_spacing.dart
@@ -17,14 +17,15 @@ class AppSpacing extends StatelessWidget {
   factory AppSpacing.v8() => const AppSpacing(v: 8);
   factory AppSpacing.h10() => const AppSpacing(h: 10);
   factory AppSpacing.v10() => const AppSpacing(v: 10);
-  factory AppSpacing.h16() => const AppSpacing(h: 16);
-  factory AppSpacing.v16() => const AppSpacing(v: 16);
-  factory AppSpacing.v12() => const AppSpacing(v: 12);
   factory AppSpacing.h12() => const AppSpacing(h: 12);
+  factory AppSpacing.v12() => const AppSpacing(v: 12);
+  factory AppSpacing.v16() => const AppSpacing(v: 16);
+  factory AppSpacing.h16() => const AppSpacing(h: 16);
   factory AppSpacing.v24() => const AppSpacing(v: 24);
   factory AppSpacing.h24() => const AppSpacing(h: 24);
-  factory AppSpacing.h32() => const AppSpacing(h: 32);
   factory AppSpacing.h30() => const AppSpacing(h: 30);
+  factory AppSpacing.v30() => const AppSpacing(v: 30);
+  factory AppSpacing.h32() => const AppSpacing(h: 32);
   factory AppSpacing.v32() => const AppSpacing(v: 32);
 
   final int v;
