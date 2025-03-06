4df30fff4ad6a400d1a33e1a350d4097d4c0b0a9
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-06 04:54:42 +0000

    Enhance SkeletonActivityCard with Shimmer loading effect

--- lib/ui/widgets/skeleton_activity_card.dart
+++ lib/ui/widgets/skeleton_activity_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SkeletonActivityCard extends StatelessWidget {
   final Color cardColor;
@@ -25,56 +26,60 @@ class SkeletonActivityCard extends StatelessWidget {
           ),
         ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Title skeleton
-          Container(
-            width: 60.w,
-            height: 10.h,
-            decoration: BoxDecoration(
-              color: Colors.grey[300],
-              borderRadius: BorderRadius.circular(4.r),
-            ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.centerLeft,
-              // Hours skeleton
-              child: Container(
-                width: 80.w,
-                height: 24.h,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(4.r),
-                ),
+      child: Shimmer.fromColors(
+        baseColor: Colors.grey[300]!,
+        highlightColor: Colors.grey[100]!,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title skeleton
+            Container(
+              width: 60.w,
+              height: 10.h,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(4.r),
               ),
             ),
-          ),
-          Row(
-            children: [
-              Container(
-                width: 6.w,
-                height: 6.w,
-                decoration: BoxDecoration(
-                  color: borderColor.withOpacity(0.3),
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(2.r),
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                // Hours skeleton
+                child: Container(
+                  width: 80.w,
+                  height: 24.h,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(4.r),
+                  ),
                 ),
               ),
-              SizedBox(width: 6.w),
-              // Completed text skeleton
-              Container(
-                width: 100.w,
-                height: 8.h,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(4.r),
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 6.w,
+                  height: 6.w,
+                  decoration: BoxDecoration(
+                    color: borderColor.withOpacity(0.3),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(2.r),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                SizedBox(width: 6.w),
+                // Completed text skeleton
+                Container(
+                  width: 100.w,
+                  height: 8.h,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(4.r),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
