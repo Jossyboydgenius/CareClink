2da4c4c97fabdd3e0d1db444f0574f7839f5c022
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-14 05:26:56 +0000

    Enhance UserModel to include additional fields and implement initials getter

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -2,17 +2,30 @@ class UserModel {
   final String? email;
   final String? password;
   final String? token;
+  final String? fullname;
+  final String? profileImage;
+  final String? id;
+  final String? role;
 
   UserModel({
     this.email,
     this.password,
     this.token,
+    this.fullname,
+    this.profileImage,
+    this.id,
+    this.role,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
+    final user = json['user'] as Map<String, dynamic>?;
     return UserModel(
-      email: json['email'],
+      email: user?['email'],
       token: json['token'],
+      fullname: user?['fullname'],
+      profileImage: user?['profileImage'],
+      id: user?['id'],
+      role: user?['role'],
     );
   }
 
@@ -21,5 +34,14 @@ class UserModel {
       'email': email,
       'password': password,
     };
+  }
+
+  String get initials {
+    if (fullname == null || fullname!.isEmpty) return '';
+    final names = fullname!.split(' ');
+    if (names.length >= 2) {
+      return '${names[0][0]}${names[1][0]}'.toUpperCase();
+    }
+    return names[0][0].toUpperCase();
   }
 } 
