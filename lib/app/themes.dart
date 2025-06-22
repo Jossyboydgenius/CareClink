b6bcf832bde37741c184356f1f52c1b8eb4de693
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-22 07:51:45 +0000

    Add Google Fonts integration to AppTheme for enhanced typography
    - Imported the Google Fonts package and applied the Outfit font to the text theme in AppTheme.
    - Updated font family settings to utilize Google Fonts for improved text styling across the application.

--- lib/app/themes.dart
+++ lib/app/themes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../shared/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ThemeData get light => ThemeData(
@@ -9,5 +10,7 @@ class AppTheme {
         ),
         scaffoldBackgroundColor: AppColors.background,
         useMaterial3: true,
+        textTheme: GoogleFonts.outfitTextTheme(),
+        fontFamily: GoogleFonts.outfit().fontFamily,
       );
-} 
+}
